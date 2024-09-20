c3c3356cc5d7d1f0e5dd45c101eae7680215d57b
UliGaMi <ulises.galvez@hotmail.com>
2024-09-20 17:38:26 -0600

    [ADD] Se agregó la lógica para subir los documentos y el archivo requirements.txt

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 def upload_form():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return 'File successfully uploaded'
+
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
